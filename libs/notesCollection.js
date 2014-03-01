7c26fb6fa8a2f926d8e21ebc9ae4691e3c1ae9ff
pyrho <pyrhockz@gmail.com>
2014-03-01 18:26:17 +0100

    Move notes refactor.

--- libs/notesCollection.js
+++ libs/notesCollection.js
@@ -15,7 +15,7 @@ module.exports.deleteUserNotes = function(userId){
   });
 };
 
-module.exports.getUserNotes = function(userId){
+function getUserNotes(userId){
   return new Promise(function(resolve, reject){
     notesCollection.findOne({userId: userId})
       .success(function(userData){
@@ -23,9 +23,10 @@ module.exports.getUserNotes = function(userId){
       })
       .on('error', reject);
   });
-};
+}
+module.exports.getUserNotes = getUserNotes;
 
-module.exports.storeUserNotes = function(userId, notes){
+function storeUserNotes(userId, notes){
   return new Promise(function(resolve, reject){
     logger.debug('Storing notes');
     notesCollection.update({
@@ -39,63 +40,69 @@ module.exports.storeUserNotes = function(userId, notes){
     .success(resolve)
     .on('error', reject);
   });
-};
+}
+module.exports.storeUserNotes = storeUserNotes;
+
+function getNoteIndex(notes, noteGuid){
+  for(var i = 0; i < notes.length; ++i){
+    if(notes[i].guid === noteGuid){
+      return i;
+    }
+  }
+  return -1;
+}
+
+function moveNote(notes, userId, oldIx, newIx){
+  var swappee = notes[newIx];
+  notes[newIx] = notes[oldIx];
+  notes[oldIx] = swappee;
+  return storeUserNotes(userId, notes);
+}
 
 
 module.exports.moveNoteDown = function(userId, noteGuid){
-  return new Promise(function(resolve, reject){
-    if(!userId || !noteGuid){
-      return reject(new Error('Invalid parameters'));
-    }
+  if(!userId || !noteGuid){
+    return Promise.reject(new Error('Invalid parameters'));
+  }
 
+  return new Promise(function(resolve, reject){
     module.exports.getUserNotes(userId)
       .error(function(e){
         return reject(new Error('Failed querying DB: ' + e));
       })
       .done(function(notes){
-        var i;
-        for(i = 0; i < notes.length; ++i){
-          if(notes[i].guid === noteGuid){
-            if(i === (notes.length - 1)){
-              return reject(new Error('Already at max index'));
-            }
-            var tmpSwap = notes[i + 1];
-            notes[i + 1] = notes[i];
-            notes[i] = tmpSwap ;
-            return resolve(module.exports.storeUserNotes(userId, notes));
-          }
-
+        var noteIndex = getNoteIndex(notes, noteGuid);
+        if(noteIndex === -1){
+          return reject(new Error('Note not found'));
         }
-        return reject(new Error('Note not found'));
+        if(noteIndex === notes.length - 1){
+          return reject(new Error('Note already at bottom'));
+        }
+        return resolve(moveNote(notes, userId, noteIndex, noteIndex + 1));
       });
   });
 };
 
 
 module.exports.moveNoteUp = function(userId, noteGuid){
-  return new Promise(function(resolve, reject){
-    if(!userId || !noteGuid){
-      return reject(new Error('Invalid parameters'));
-    }
+  if(!userId || !noteGuid){
+    return Promise.reject(new Error('Invalid parameters'));
+  }
 
+  return new Promise(function(resolve, reject){
     module.exports.getUserNotes(userId)
       .error(function(e){
-        return reject(new Error('Failed querying DB' + e));
+        return reject(new Error('Failed querying DB: ' + e));
       })
       .done(function(notes){
-        var i;
-        for(i = 0; i < notes.length; ++i){
-          if(notes[i].guid === noteGuid){
-            if(i === 0){
-              return reject(new Error('Already at index 0'));
-            }
-            var tmpSwap = notes[i - 1];
-            notes[i-1] = notes[i];
-            notes[i] = tmpSwap ;
-            return resolve(module.exports.storeUserNotes(userId, notes));
-          }
+        var noteIndex = getNoteIndex(notes, noteGuid);
+        if(noteIndex === -1){
+          return reject(new Error('Note not found'));
         }
-        return reject(new Error('Note not found'));
+        if(noteIndex === 0){
+          return reject(new Error('Note already at top'));
+        }
+        return resolve(moveNote(notes, userId, noteIndex, noteIndex - 1));
       });
   });
 };
